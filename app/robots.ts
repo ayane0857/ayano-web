d7136be21995dcdf48cec976a8c5431bd2c2105c
Shinahin <tailangrongyi@gmail.com>
2025-07-07 18:24:50 +0900

    クローラー周りの修正

--- app/robots.ts
+++ app/robots.ts
@@ -1,11 +1,11 @@
-import type { MetadataRoute } from 'next'
+import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
-      userAgent: '*',
-      allow: '/',
+      userAgent: "*",
+      allow: "/",
     },
-    sitemap: 'https://ayano.uniproject.jp/sitemap.xml',
-  }
+    sitemap: "https://ayano.ayane0857.net/sitemap.xml",
+  };
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,17 +1,18 @@
-import type { MetadataRoute } from 'next'
+import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-return [
+  return [
     {
-        url: 'https://ayano.uniproject.jp/',
-        lastModified: new Date(),
-        changeFrequency: 'yearly',
-        priority: 1,
+      url: "https://ayano.ayane0857.net/",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
     },
     {
-        url: 'https://ayano.uniproject.jp/commands',
-        lastModified: new Date(),
-        changeFrequency: 'weekly',
-        priority: 0.8,
+      url: "https://ayano.ayane0857.net/commands",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
     },
-]}
+  ];
+}
